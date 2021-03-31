7e2315b5f3616f575a413a9aa29c8cc4e6f17cb3
wlrhkd49 <wlrhkd49@naver.com>
2021-03-31 16:33:15 +0900

    Account Profile Api

---
+++ src/main/java/com/cau/cc/api/AccountProfileApiController.java
@@ -0,0 +1,41 @@
+package com.cau.cc.api;
+
+import com.cau.cc.ifs.CrudInterface;
+import com.cau.cc.model.network.Header;
+import com.cau.cc.model.network.request.AccountApiRequest;
+import com.cau.cc.model.network.response.AccountApiResponse;
+import com.cau.cc.service.AccountProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/profile")
+public class AccountProfileApiController implements CrudInterface<AccountApiRequest, AccountApiResponse> {
+
+    @Autowired
+    private AccountProfileService accountProfileService;
+
+    @Override
+    @PostMapping("") // /api/profile
+    public Header<AccountApiResponse> create(Header<AccountApiRequest> request) {
+        return accountProfileService.create(request);
+    }
+
+    @Override
+    @GetMapping("{id}") // /api/profile/id
+    public Header<AccountApiResponse> read(Long id) {
+        return accountProfileService.read(id);
+    }
+
+    @Override
+    @PostMapping("") // /api/profile
+    public Header<AccountApiResponse> update(Header<AccountApiRequest> request) {
+        return accountProfileService.update(request);
+    }
+
+    @Override
+    @DeleteMapping("{id}") // /api/profile/id
+    public Header delete(Long id) {
+        return accountProfileService.delete(id);
+    }
+}

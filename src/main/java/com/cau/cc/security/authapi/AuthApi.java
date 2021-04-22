30e0e3dce0980ffd2488a4a8bf7cb778f3dbc784
hwangyoungjin <innovation950302@gmail.com>
2021-04-23 01:41:43 +0900

    logout 기능 구현

---
+++ src/main/java/com/cau/cc/security/authapi/AuthApi.java
@@ -0,0 +1,36 @@
+package com.cau.cc.security.authapi;
+
+import com.cau.cc.model.entity.Account;
+import com.cau.cc.model.network.Header;
+import com.cau.cc.model.network.request.AccountApiRequest;
+import com.cau.cc.model.network.response.LogoutApiResponse;
+import com.cau.cc.model.repository.AccountRepository;
+import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * API용도
+ * spring security filter가 가로채서 처리 하므로 실행되지 않는다.
+ */
+@Api(tags = "로그인 & 로그아웃")
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+@RestController
+public class AuthApi {
+
+    @ApiOperation(value = "로그인",notes = "이메일과 password만 입력")
+    @PostMapping("/login")
+    public void fakeLogin(@ApiParam(value = "email & password") @RequestBody AccountApiRequest accountApiRequest) {
+        throw new IllegalStateException("This method shouldn't be called. It's implemented by Spring Security filters.");
+    }
+
+    @ApiOperation(value = "로그아웃")
+    @PostMapping("/logout")
+    public void fakeLogin() {
+        throw new IllegalStateException("This method shouldn't be called. It's implemented by Spring Security filters.");
+    }
+}
+
---
+++ src/main/java/com/cau/cc/security/handler/CustomLogourSuccessHandler.java
@@ -0,0 +1,33 @@
+package com.cau.cc.security.handler;
+
+import com.cau.cc.model.network.Header;
+import com.cau.cc.model.network.response.LoginApiResponse;
+import com.cau.cc.model.network.response.LogoutApiResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomLogourSuccessHandler implements LogoutSuccessHandler {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        LogoutApiResponse logoutApiResponse = LogoutApiResponse.builder()
+                .result(true)
+                .build();
+
+        //response
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        objectMapper.writeValue(response.getWriter(), Header.OK(logoutApiResponse));
+    }
+}

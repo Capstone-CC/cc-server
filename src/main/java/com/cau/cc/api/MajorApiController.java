0f2c9955b9bb8e8773a3047ce92d8ed0638d9b93
wlrhkd49 <wlrhkd49@naver.com>
2021-05-04 14:20:27 +0900

    remove major

--- src/main/java/com/cau/cc/api/MajorApiController.java
+++ src/main/java/com/cau/cc/api/MajorApiController.java
@@ -1,57 +1,57 @@
-package com.cau.cc.api;
-
-import com.cau.cc.ifs.CrudInterface;
-import com.cau.cc.model.entity.MajorEnum;
-import com.cau.cc.model.network.Header;
-import com.cau.cc.model.network.request.MajorApiRequest;
-import com.cau.cc.model.network.response.MajorApiResponse;
-import com.cau.cc.service.MajorApiLogicService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.w3c.dom.stylesheets.LinkStyle;
-
-@RestController
-@Api(tags = "major")
-@RequestMapping("/major")
-public class MajorApiController implements CrudInterface<MajorApiRequest, MajorApiResponse> {
-
-    @Autowired
-    private MajorApiLogicService majorApiLogicService;
-
-    @Override
-    @PostMapping("") // /api/major
-    public Header<MajorApiResponse> create(@RequestBody MajorApiRequest request) {
-        return majorApiLogicService.create(request);
-    }
-
-    @Override
-    @GetMapping("{id}") // /api/major/id
-    public Header<MajorApiResponse> read(@PathVariable Long id) {
-        return majorApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("") // /api/major
-    public Header<MajorApiResponse> update(@RequestBody MajorApiRequest request) {
-        return majorApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}") // /api/major/id
-    public Header delete(@PathVariable Long id) {
-        return majorApiLogicService.delete(id);
-    }
-
-    @GetMapping("/list")
-    @ApiOperation(value = "major list",notes = "major list")
-    public Header<MajorApiResponse> getMajors(){
-        MajorApiResponse majorApiResponse = new MajorApiResponse();
-
-        for(MajorEnum m : MajorEnum.values()){
-            majorApiResponse.getMajorEnums().add(m);
-        }
-        return Header.OK(majorApiResponse);
-    }
-}
+//package com.cau.cc.api;
+//
+//import com.cau.cc.ifs.CrudInterface;
+//import com.cau.cc.model.entity.MajorEnum;
+//import com.cau.cc.model.network.Header;
+//import com.cau.cc.model.network.request.MajorApiRequest;
+//import com.cau.cc.model.network.response.MajorApiResponse;
+//import com.cau.cc.service.MajorApiLogicService;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//import org.w3c.dom.stylesheets.LinkStyle;
+//
+//@RestController
+//@Api(tags = "major")
+//@RequestMapping("/major")
+//public class MajorApiController implements CrudInterface<MajorApiRequest, MajorApiResponse> {
+//
+//    @Autowired
+//    private MajorApiLogicService majorApiLogicService;
+//
+//    @Override
+//    @PostMapping("") // /api/major
+//    public Header<MajorApiResponse> create(@RequestBody MajorApiRequest request) {
+//        return majorApiLogicService.create(request);
+//    }
+//
+//    @Override
+//    @GetMapping("{id}") // /api/major/id
+//    public Header<MajorApiResponse> read(@PathVariable Long id) {
+//        return majorApiLogicService.read(id);
+//    }
+//
+//    @Override
+//    @PutMapping("") // /api/major
+//    public Header<MajorApiResponse> update(@RequestBody MajorApiRequest request) {
+//        return majorApiLogicService.update(request);
+//    }
+//
+//    @Override
+//    @DeleteMapping("{id}") // /api/major/id
+//    public Header delete(@PathVariable Long id) {
+//        return majorApiLogicService.delete(id);
+//    }
+//
+//    @GetMapping("/list")
+//    @ApiOperation(value = "major list",notes = "major list")
+//    public Header<MajorApiResponse> getMajors(){
+//        MajorApiResponse majorApiResponse = new MajorApiResponse();
+//
+//        for(MajorEnum m : MajorEnum.values()){
+//            majorApiResponse.getMajorEnums().add(m);
+//        }
+//        return Header.OK(majorApiResponse);
+//    }
+//}

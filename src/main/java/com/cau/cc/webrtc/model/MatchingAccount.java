0bdbd4af06da0aec10bd565b39910218968ddf5b
hwangyoungjin <innovation950302@gmail.com>
2021-05-11 14:34:57 +0900

    update : matchingroom 생성

--- src/main/java/com/cau/cc/webrtc/model/MatchingAccount.java
+++ src/main/java/com/cau/cc/webrtc/model/MatchingAccount.java
@@ -14,6 +14,7 @@ import org.springframework.web.socket.WebSocketSession;
 @Builder
 public class MatchingAccount {
 
+    private Long id;
     private WebSocketSession mySession;
     private int grade; // 학년
     private String email;

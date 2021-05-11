96ae527126dc2a1139d94bb6414a833d706274d2
wlrhkd49 <wlrhkd49@naver.com>
2021-05-11 22:37:00 +0900

    upload max file size

--- src/main/java/com/cau/cc/CcApplication.java
+++ src/main/java/com/cau/cc/CcApplication.java
@@ -1,6 +1,5 @@
 package com.cau.cc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
@@ -8,11 +7,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.util.unit.DataSize;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.servlet.MultipartConfigElement;
-import javax.sql.DataSource;
-import java.sql.Connection;
 
 @SpringBootApplication
 @EnableScheduling
@@ -26,10 +22,8 @@ public class CcApplication {
     @Bean
     MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
-        factory.setMaxFileSize(DataSize.ofMegabytes(1024));
-        factory.setMaxRequestSize(DataSize.ofMegabytes(1024));
+        factory.setMaxFileSize(DataSize.ofMegabytes(5));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(5));
         return factory.createMultipartConfig();
     }
-
-
 }

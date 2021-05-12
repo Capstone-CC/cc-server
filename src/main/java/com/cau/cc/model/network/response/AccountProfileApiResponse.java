4918f2844ef2aff7095aaf0fee44ae22f026938b
wlrhkd49 <wlrhkd49@naver.com>
2021-05-12 13:44:28 +0900

    upload showing chatlist

--- src/main/java/com/cau/cc/model/network/response/AccountProfileApiResponse.java
+++ src/main/java/com/cau/cc/model/network/response/AccountProfileApiResponse.java
@@ -32,6 +32,6 @@ public class AccountProfileApiResponse {
 
     private String content;
 
-    private List<Chatroom> chatroomApiResponseList;
+    private List<ChatroomApiResponse> chatroomApiResponseList;
 
 }

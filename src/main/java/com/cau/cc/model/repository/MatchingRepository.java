8a19b3379d049f509b39100dc0456dd4e7166d20
hwangyoungjin <innovation950302@gmail.com>
2021-05-13 15:47:05 +0900

    Update : Service, Repository에 시간, man id, womanid로 matching 조회 추가

--- src/main/java/com/cau/cc/model/repository/MatchingRepository.java
+++ src/main/java/com/cau/cc/model/repository/MatchingRepository.java
@@ -2,8 +2,15 @@ package com.cau.cc.model.repository;
 
 import com.cau.cc.model.entity.Matching;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface MatchingRepository extends JpaRepository<Matching, Long> {
+
+    @Query("SELECT m FROM Matching m  WHERE m.manId.id = :mId and m.womanId.id = :wId and m.time = :time")
+    Matching findByManIdAndWomanIdAndTime(@Param("mId") Long mId,@Param("wId") Long wId,@Param("time") LocalDateTime time);
 }

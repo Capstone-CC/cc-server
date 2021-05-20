d8f1783025a1e538311ad3c24bfe4e1279362f53
wlrhkd49 <wlrhkd49@naver.com>
2021-05-20 15:01:39 +0900

    update Runner

--- src/main/java/com/cau/cc/Runner.java
+++ src/main/java/com/cau/cc/Runner.java
@@ -45,33 +45,33 @@ public class Runner implements ApplicationRunner {
 //
 //
 //            test db
-            Account account2 = Account.builder()
-            //        .id(2l)
-                    .email("test1")
-                    .password(passwordEncoder.encode("123123"))
-                    .gender(GenderEnum.남)
-                    .majorName(MajorEnum.생명과학과)
-                    .nickName("푸앙이")
-                    .grade(1)
-                    .count(4)
-                    .image("https://caucampuscontact.s3.amazonaws.com/images/logo.png")
-                    .build();
-
-            accountRepository.save(account2);
-
-            Account account3 = Account.builder()
-                    //        .id(2l)
-                    .email("test2")
-                    .password(passwordEncoder.encode("123123"))
-                    .gender(GenderEnum.여)
-                    .majorName(MajorEnum.수학과)
-                    .grade(3)
-                    .count(4)
-                    .nickName("푸앙이")
-                    .image("https://caucampuscontact.s3.amazonaws.com/images/logo.png")
-                    .build();
-
-            accountRepository.save(account3);
+//            Account account2 = Account.builder()
+//            //        .id(2l)
+//                    .email("test1")
+//                    .password(passwordEncoder.encode("123123"))
+//                    .gender(GenderEnum.남)
+//                    .majorName(MajorEnum.생명과학과)
+//                    .nickName("푸앙이")
+//                    .grade(1)
+//                    .count(4)
+//                    .image("https://caucampuscontact.s3.amazonaws.com/images/logo.png")
+//                    .build();
+//
+//            accountRepository.save(account2);
+//
+//            Account account3 = Account.builder()
+//                    //        .id(2l)
+//                    .email("test2")
+//                    .password(passwordEncoder.encode("123123"))
+//                    .gender(GenderEnum.여)
+//                    .majorName(MajorEnum.수학과)
+//                    .grade(3)
+//                    .count(4)
+//                    .nickName("푸앙이")
+//                    .image("https://caucampuscontact.s3.amazonaws.com/images/logo.png")
+//                    .build();
+//
+//            accountRepository.save(account3);
 
 
 

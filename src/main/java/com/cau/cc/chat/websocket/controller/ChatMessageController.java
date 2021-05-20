b5ab82ded0fee90c797680f97b58c29f24987dd9
hwangyoungjin <innovation950302@gmail.com>
2021-05-20 14:41:11 +0900

    Add : chat Config

--- src/main/java/com/cau/cc/chat/websocket/controller/ChatMessageController.java
+++ src/main/java/com/cau/cc/chat/websocket/controller/ChatMessageController.java
@@ -4,6 +4,7 @@ import com.cau.cc.chat.websocket.ChatRoom;
 import com.cau.cc.chat.websocket.chatmessage.ChatMessage;
 import com.cau.cc.chat.websocket.service.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
@@ -12,18 +13,16 @@ import java.util.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/chat")
-public class ChatController {
+public class ChatMessageController {
 
     private final ChatService chatService;
 
-    @PostMapping
-    public ChatRoom createRoom(@RequestParam String name) {
-        return chatService.createRoom(name);
-    }
+    private final SimpMessageSendingOperations messagingTemplate;
 
-    @GetMapping
-    public List<ChatRoom> findAllRoom() {
-        return chatService.findAllRoom();
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message){
+        if(ChatMessage.MessageType.TALK.equals(message.getType())){
+            messagingTemplate.convertAndSend("/sub/chat/room/"+message.getRoomId(),message);
+        }
     }
 }
---
+++ src/main/java/com/cau/cc/chat/websocket/controller/ChatRoomController.java
@@ -0,0 +1,15 @@
+package com.cau.cc.chat.websocket.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
+public class ChatRoomController {
+
+    /**
+     * 채팅방 입장
+     */
+}

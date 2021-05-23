88502ca2c2cf659560421c583918fbc648fa582d
wlrhkd49 <wlrhkd49@naver.com>
2021-05-23 17:19:39 +0900

    show last message

--- src/main/java/com/cau/cc/model/repository/ChatMessageRepository.java
+++ src/main/java/com/cau/cc/model/repository/ChatMessageRepository.java
@@ -17,6 +17,7 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> 
 //    @Query("SELECT u From ChatMessage u WHERE u.chatroomId.id = ?1 order by u.id DESC limit=1")
 //    ChatMessage findByChatroomId(Long id);
 
-    @Query("SELECT u FROM ChatMessage u  WHERE u.chatroomId.id = ?1 and u.id = max(u.id) group by u.id")
+    @Query("SELECT u FROM ChatMessage u where u.id = (select max(s.id) from ChatMessage s where s.chatroomId.id = ?1 )")
     ChatMessage findLastMessage(Long id);
+
 }

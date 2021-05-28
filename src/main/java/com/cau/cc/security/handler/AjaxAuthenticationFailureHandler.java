e2a0159539c3bb3eb797cc185ce1b04802d157cb
hwangyoungjin <innovation950302@gmail.com>
2021-05-28 18:34:54 +0900

    Modified : 로그인 실패 예외별 메세지 출력

--- src/main/java/com/cau/cc/security/handler/AjaxAuthenticationFailureHandler.java
+++ src/main/java/com/cau/cc/security/handler/AjaxAuthenticationFailureHandler.java
@@ -1,10 +1,13 @@
 package com.cau.cc.security.handler;
 
 import com.cau.cc.model.network.Header;
+import com.cau.cc.security.exception.UserSuspensionException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import javax.servlet.ServletException;
@@ -33,7 +36,14 @@ public class AjaxAuthenticationFailureHandler implements AuthenticationFailureHa
 //                .build();
         response.setStatus(HttpStatus.SC_OK);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
 
-        objectMapper.writeValue(response.getWriter(), Header.ERROR("Login failed"));
+        if (exception instanceof UsernameNotFoundException) {
+            objectMapper.writeValue(response.getWriter(), Header.ERROR("존재하지 않는 이메일입니다."));
+        } else if (exception instanceof BadCredentialsException) {
+            objectMapper.writeValue(response.getWriter(), Header.ERROR("비밀번호가 일치하지 않습니다."));
+        } else if (exception instanceof UserSuspensionException) {
+            objectMapper.writeValue(response.getWriter(), Header.ERROR("정지된 계정입니다."));
+        }
     }
 }
